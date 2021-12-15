a79d7b2d0401b6c4e8efa46aa63987e73ab7019b
mingwang <nightwind@atguigu.com>
2021-12-15 11:54:03 +0800

    create a new java test file11

---
+++ src/main/java/com/atguigu/chapter07/FlinkWindows.java
@@ -0,0 +1,66 @@
+package com.atguigu.chapter07;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class FlinkWindows {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        conf.setInteger("rest.port",20000);
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.setParallelism(1);
+
+        DataStreamSource<String> dss = env.socketTextStream("hadoop218", 9999);
+
+        dss.flatMap(new FlatMapFunction<String, Tuple2<String,Long>>() {
+
+            @Override
+            public void flatMap(String s, Collector<Tuple2<String, Long>> out) throws Exception {
+
+                for (String words : s.split(" ")) {
+                    out.collect(Tuple2.of(words,1L));
+                }
+            }
+        })
+                .keyBy(t->t.f0)
+                        .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                                .process(new ProcessWindowFunction<Tuple2<String, Long>, String, String, TimeWindow>() {
+                                    @Override
+                                    public void process(String key,
+                                                        Context ctx,
+                                                        Iterable<Tuple2<String, Long>> elements,
+                                                        Collector<String> out) throws Exception {
+
+                                        Date start = new Date(ctx.window().getStart());
+
+                                        Date end = new Date(ctx.window().getEnd());
+
+                                        ArrayList<String> words = new ArrayList<>();
+
+                                        for (Tuple2<String, Long> element : elements) {
+                                            words.add(element.f0);
+                                        }
+
+                                        out.collect("窗口" + start + " "+ end + " "+ words);
+
+                                    }
+                                })
+                                        .print();
+        env.execute();
+    }
+}
